fb316ffa7af29ed959ab67ebbd1f1650867ebc62
Rafael Leal <rpsantos@iportsolutions.com.br>
2025-04-29 08:57:21 -0300

    Até aula 27

--- 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -1,7 +1,12 @@
+//Comentário 1
+
 const FirstComponent = () => 
 {
+//Comentário 2
+
     return (
         <div>
+            {/*Comentário 3*/}
             <h1>Meu primeiro componente</h1>
         </div>
     )
