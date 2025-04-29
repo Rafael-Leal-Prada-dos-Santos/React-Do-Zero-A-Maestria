2bec3fd390fe749e6595a689626297e06411d94d
Rafael Leal <rpsantos@iportsolutions.com.br>
2025-04-29 08:46:38 -0300

    Aulas ate 25

--- 2_FUNDAMENTOS/fundamentos/src/App.js
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -1,23 +1,16 @@
+import FirstComponent from './components/FirstComponent';
+
+//Imagem
 import logo from './logo.svg';
+
+//styles /CSS
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Fundamentos de React</h1>
+     <FirstComponent/>
     </div>
   );
 }
