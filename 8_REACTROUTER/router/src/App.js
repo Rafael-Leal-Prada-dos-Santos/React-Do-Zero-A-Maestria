4db06512ec25018c6c36914fe6d56b5623c7e788
Rafael <rafalealpsantos@gmail.com>
2025-05-12 23:34:05 -0300

    Ate aula 107

--- 8_REACTROUTER/router/src/App.js
+++ 8_REACTROUTER/router/src/App.js
@@ -2,17 +2,21 @@ import './App.css';
 
 //1 - config react router
 
-import {BrowserRouter , Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
-      <BrowserRouter>
+      <Navbar/>
       <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
-      </BrowserRouter>
     </div>
   );
 }
---
+++ 8_REACTROUTER/router/src/pages/Home.js
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom"
+import { useFetch } from "../hooks/useFetch"
+
+import "./Home.css"
+
+const Home = () => {
+
+  const url = "http://localhost:3000/products"
+
+   const {data: products, loading, error} = useFetch(url);
+
+  return (
+    <div>
+        {error && <p>Houve um erro!</p>}
+        {loading && <p>Carregando, por favor Aguarde</p>}
+        {!loading && (
+            <ul>
+                { products && products.map((product) => (
+                    <li key={product.id}>
+                        <h2>{product.name}</h2>
+                        <p>R${product.price}</p>
+                    </li>)) 
+                }
+            </ul>
+        )} 
+    </div>
+  )
+}
+
+export default Home
