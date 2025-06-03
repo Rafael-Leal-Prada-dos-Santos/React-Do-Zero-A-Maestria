5a30813b2d0fbb72ab2be4be978d1d6cf4e7dac6
Rafael <rafalealpsantos@gmail.com>
2025-06-03 00:07:34 -0300

    Iniico sessao 9

---
+++ 9_CONTEXTAPI/contextapi/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import NavBar from './components/NavBar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Context API</h1>
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
